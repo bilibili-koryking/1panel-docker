10fc1e6202d0e7bb45c5b9b175467087375e9ed3
koryking <516063266@qq.com>
2024-07-19 00:15:53 +0800

    修改更新版本逻辑

--- update_app_version.sh
+++ update_app_version.sh
@@ -2,21 +2,21 @@
 
 # 更新数据库
 update_database() {
-  if [[ -f /opt/1panel/db/1Panel.db ]]; then
+  if [[ -f ${PANEL_BASE_DIR}/1panel/db/1Panel.db ]]; then
     # 备份数据库文件
-    cp /opt/1panel/db/1Panel.db /opt/1panel/db/1Panel.db.bak
+    cp ${PANEL_BASE_DIR}/1panel/db/1Panel.db ${PANEL_BASE_DIR}/1panel/db/1Panel.db.bak
 
     # 使用 sqlite3 执行更新操作
-    sqlite3 /opt/1panel/db/1Panel.db <<EOF
+    sqlite3 ${PANEL_BASE_DIR}/1panel/db/1Panel.db <<EOF
 UPDATE settings
-SET value = '$PANELVER'
+SET value = '$(curl -s https://resource.fit2cloud.com/1panel/package/stable/latest)'
 WHERE key = 'SystemVersion';
 .exit
 EOF
 
-    echo "数据库版本已更新为 $PANELVER"
+    echo "数据库版本已更新为 $(curl -s https://resource.fit2cloud.com/1panel/package/stable/latest)"
   else
-    echo "警告：/opt/1panel/db/1Panel.db 文件不存在" >&2
+    echo "警告：${PANEL_BASE_DIR}/1panel/db/1Panel.db 文件不存在" >&2
     exit 0
   fi
 }
